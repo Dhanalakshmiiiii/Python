12c5b70518963980cd0f28cc5ae616f61ecbb98f
Dhanalakshmiiiii <150053848+Dhanalakshmiiiii@users.noreply.github.com>
2023-12-31 00:10:00 +0530

    Create sparse.py

---
+++ sparse.py
@@ -0,0 +1,15 @@
+a=[]
+print("Square Matrix")
+n=int(input("Enter the rows and columns:"))
+b={}
+a=[[]*n for i in range(n)]
+print("\nEnter the elements of matrix:")
+for i in range(0,n):
+  for j in range(0,n):
+    a[i].append(int(input()))
+print("\nGiven matrix:\n",a)
+for i in range(0,n):
+  for j in range(0,n):
+    if a[i][j]!=0:
+      b[(i,j)]=a[i][j]
+print("Dictionary is:",b)
