b2a56bd89df7251b8e88216884e9619a16db690c
Dhanalakshmiiiii <150053848+Dhanalakshmiiiii@users.noreply.github.com>
2023-12-31 00:42:55 +0530

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,12 @@
+user-interactive:
+
+x=int(input("Enter first number:"))
+y=int(input("Enter second number:"))
+sum=x+y
+print("Sum of the numbers:",sum)
+
+by taking default value:
+a=10
+b=120
+sum=a+b
+print("Sum:",sum)
